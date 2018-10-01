f30b510735f9cc27f4aeaa175d47488ac26b3f5a
Daria-V <zmey10.1989@mail.ru>
2018-10-01 23:13:11 +0300

    Уровень 1. Урок 4. Свидлова Д

---
+++ lvl1_lsn4/lvl1_lsn4/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lvl1_lsn4
+{
+    public struct Account
+    {
+        public string login;
+        public string password;
+    }
+}
